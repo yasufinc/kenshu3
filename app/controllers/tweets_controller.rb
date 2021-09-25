ad8b8f9064bf4f111646e5309c47cd8f8b53eeb9
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-26 02:11:45 +0900

    Tweets/createのコントローラー作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,10 +5,34 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
   end
 
-  # private
-  #   def set_twitter
-  #     @twitter = Twitter.find(params[:id])
-  #   end
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    #@tweet = MyThread.create(tweet_params)
+    #@tweet = MyThread.new(tweet_params)
+    # binding.pry
+    @tweet = current_user.tweets.build(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path, notice: "新規Tweetが作成されました"
+    else
+      flash.now[:alert] = "失敗しました"
+      render :new #new.html.erbに飛んでくれる
+    end
+
+  end
+
+
+
+  private
+    def set_twitter
+      @twitter = Twitter.find(params[:id])
+    end
+
+    def tweet_params
+      params.require(:tweet).permit(:content, :publicity)
+    end
 
 
 
