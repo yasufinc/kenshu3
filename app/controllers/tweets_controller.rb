4916cd24f1faec20f2961c0c0bd709f659a6e13e
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-25 20:10:12 +0900

    tweets/indexに対応するビューとコントローラーの作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,20 @@
 class TweetsController < ApplicationController
+  #before_action :set_twitter
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  # private
+  #   def set_twitter
+  #     @twitter = Twitter.find(params[:id])
+  #   end
+
+
+
+
+
+
+
+
 end
