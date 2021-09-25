d9a1dcd312a80270fed6df13c4abc11251f528b3
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-26 02:10:59 +0900

    Userモデルと、Tweetモデルの関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_25_032436) do
+ActiveRecord::Schema.define(version: 2021_09_25_165424) do
 
   create_table "tweets", charset: "utf8mb4", force: :cascade do |t|
     t.string "content", null: false
     t.integer "publicity", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
