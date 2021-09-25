a0e951d294dd42ad7ab9795d5bf3de204f4eb0e6
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-25 19:55:09 +0900

    不要部分削除

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,7 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # resources :my_threads do
-  #   resources :comments, only: [:create, :edit, :update, :destroy]
-  # end
-
   resources :tweets
-
 
   root 'tweets#index'
 end
