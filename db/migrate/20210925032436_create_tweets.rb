2a5f5e80a075351df91d9507351bd6277e19a181
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-26 11:38:01 +0900

    added default

--- db/migrate/20210925032436_create_tweets.rb
+++ db/migrate/20210925032436_create_tweets.rb
@@ -1,8 +1,8 @@
 class CreateTweets < ActiveRecord::Migration[6.1]
   def change
     create_table :tweets do |t|
-      t.string :content, null: false
-      t.integer :publicity, null: false
+      t.string :content, null: false, default: ""
+      t.integer :publicity, null: false, default: 0
 
       t.timestamps
     end
