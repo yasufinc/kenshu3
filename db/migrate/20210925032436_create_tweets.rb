10102f1fbf4803b0692d0d70a55a1555720d0518
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-26 16:36:31 +0900

    デフォルト値を取り消し

--- db/migrate/20210925032436_create_tweets.rb
+++ db/migrate/20210925032436_create_tweets.rb
@@ -1,7 +1,7 @@
 class CreateTweets < ActiveRecord::Migration[6.1]
   def change
     create_table :tweets do |t|
-      t.string :content, null: false, default: ""
+      t.string :content, null: false
       t.integer :publicity, null: false, default: 0
 
       t.timestamps
