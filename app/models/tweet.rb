109ac5a8728d84e16b1b0533f6b6f099150e6b3c
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-26 13:47:07 +0900

    conflict解消

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,10 +1,23 @@
 class Tweet < ApplicationRecord
 
-  belongs_to :user
+  has_many_attached :images
 
+  MAX_IMAGES_COUNT = 3
+
+  validate :images_count_limit
+
+  private
+
+    def images_count_limit
+      errors.add(:base, "写真は#{MAX_IMAGES_COUNT}枚まで") if self.images.count > MAX_IMAGES_COUNT
+    end
+
+<<<<<<< HEAD
   enum publicity: {Public: 0, Follower_only: 1, Private: 2}
 
 
 
 
+=======
+>>>>>>> 65669a7 (写真投稿機能作成)
 end
