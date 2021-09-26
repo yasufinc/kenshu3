65669a7158818085d52d38ecd061b63fbb47ad1c
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-26 13:47:07 +0900

    写真投稿機能作成

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,15 @@
 class Tweet < ApplicationRecord
 
-  belongs_to :user
+  has_many_attached :images
 
-  
+  MAX_IMAGES_COUNT = 3
+
+  validate :images_count_limit
+
+  private
+
+    def images_count_limit
+      errors.add(:base, "写真は#{MAX_IMAGES_COUNT}枚まで") if self.images.count > MAX_IMAGES_COUNT
+    end
+
 end
