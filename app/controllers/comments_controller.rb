97053c1d01bfbff30798eb4d38dc9c91e7a3ed7b
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-27 12:15:54 +0900

    Comments/viewsのコントローラーを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_tweet
+  before_action :set_comment, only: %i[ edit update destroy ]
 
   def create
     @comment = current_user.comments.build(content: comment_params[:content], tweet: @tweet)
@@ -7,10 +8,25 @@ class CommentsController < ApplicationController
     redirect_to @tweet, notice: '新しいコメントが作成されました'
   end
 
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to @tweet, notice: 'コメントが正しく変更されました'
+    else
+      render 'edit', notice: 'コメントの変更に失敗しました。'
+    end
+  end
+
   private
 
     def set_tweet
       @tweet = Tweet.find(params[:tweet_id])
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
     end
 
 
