e3a6f0a687a4288879d56be319f31add15818478
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-27 22:34:27 +0900

    avoid tweet blank

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,12 +7,11 @@ class Tweet < ApplicationRecord
   MAX_IMAGES_COUNT = 3
 
   validate :images_count_limit
+  validates :content, presence: true 
 
   private
 
     def images_count_limit
       errors.add(:base, "写真は#{MAX_IMAGES_COUNT}枚まで") if self.images.count > MAX_IMAGES_COUNT
     end
-
-
 end
