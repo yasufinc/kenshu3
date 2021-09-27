344852462fa23aa05c8b849d59c0eca83eaa17e8
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-27 20:22:04 +0900

    要素揃え

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,8 @@
 class Tweet < ApplicationRecord
 
   has_many :comments, dependent: :destroy
-  belongs_to :user
   has_many_attached :images, dependent: :destroy
+  belongs_to :user
   enum publicity: {everybody: 0, only_followers: 1, only_me: 2}
 
   MAX_IMAGES_COUNT = 3
