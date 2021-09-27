e3b79fd5e491ff61c5f9734a1a7e70e13831f13a
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-27 23:15:42 +0900

    set routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  #devise_for :users
-
 
   devise_for :users, controllers: {
     sessions: 'users/sessions',
@@ -9,6 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :tweets
+  resources :users, only: [:show]
 
   root 'tweets#index'
 end
