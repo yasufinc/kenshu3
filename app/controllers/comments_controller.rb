cd00c05516bc01a714375e376f1123203a97724a
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-27 11:46:11 +0900

    Comments/create作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+  before_action :set_tweet
+
+  def create
+    @comment = current_user.comments.build(content: comment_params[:content], tweet: @tweet)
+    @comment.save
+    redirect_to @tweet, notice: '新しいコメントが作成されました'
+  end
+
+  private
+
+    def set_tweet
+      @tweet = Tweet.find(params[:tweet_id])
+    end
+
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+
+
+
 end
