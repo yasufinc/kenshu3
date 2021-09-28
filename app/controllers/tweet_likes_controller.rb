046cfa8e858c406d104996abc8b26192e181cb50
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-28 13:52:36 +0900

    before_action削除

--- app/controllers/tweet_likes_controller.rb
+++ app/controllers/tweet_likes_controller.rb
@@ -1,6 +1,5 @@
 class TweetLikesController < ApplicationController
     before_action :set_tweet
-    before_action :set_tweet_like
 
     def create
         TweetLike.create(user_id: current_user.id, tweet_id: @tweet.id)
@@ -8,6 +7,7 @@ class TweetLikesController < ApplicationController
     end
 
     def destroy
+        @tweet_like = TweetLike.find_by(user_id: current_user.id, tweet_id: @tweet.id)
         @tweet_like&.delete
         redirect_back fallback_location: root_path
     end
@@ -16,9 +16,5 @@ class TweetLikesController < ApplicationController
 
     def set_tweet
         @tweet = Tweet.find(params[:tweet_id])
-    end
-
-    def set_tweet_like
-        @tweet_like = TweetLike.find_by(user_id: current_user.id, tweet_id: @tweet.id)
     end
 end
