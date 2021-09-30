c3de2c383e0f82efb745a7707de948b21ccd00d7
Yasuhiro Matsumoto <86039223+yasufinc@users.noreply.github.com>
2021-09-30 23:18:31 +0900

    Update app/controllers/application_controller.rb
    Co-authored-by: Ryusei Ikezawa <44157470+RyuseiIkezawa@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
-  # rescue_from ::CanCan::AccessDenied do
-  #   # redirect_to tweets_path
-  #   render 'new'
-  # end
 end
