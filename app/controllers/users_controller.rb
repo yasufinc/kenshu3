511e176d375820dede83daf8287b025323c11813
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-30 16:19:22 +0900

    Viewからモデルにアクセスしないようにする。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     def show
       @tweets = Tweet.where(user_id: params[:id]).order(id: "DESC") ## idの降順
       @user = User.find(params[:id])
+      @following_or_not = @user.followed_by?(current_user)
     end
 
 end
