a3c278cafc888d78441db5efb0c02b06854f97a5
yasufinc <yasuhiro.matsumoto@finc.com>
2021-09-30 10:50:49 +0900

    find_byからfindへ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
     def show
       @tweets = Tweet.where(user_id: params[:id]).order(id: "DESC") ## idの降順
-      @user = User.find_by(id: params[:id])
+      @user = User.find(params[:id])
     end
 
 end
